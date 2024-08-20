c62d588b50ad14a7f1b1b6f5f5c174117339fa2d
Bipolar-byte <Dimanza33@yandex.ru>
2024-08-20 17:21:57 +0300

    Add files via upload

---
+++ Leason2.py
@@ -0,0 +1,5 @@
+lesson = 'Python'
+The_number_of_completed_DZ = '12'
+Number_of_hours_spent = "1.5"
+Time_for_one_task = '0.125'
+print("Курс",lesson,'всего задач:',The_number_of_completed_DZ,'затрачено часов:',Number_of_hours_spent,'среднее время выполнения:',Time_for_one_task)
