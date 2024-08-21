776d36a06aab069e4bc8f650129ec7da229aabc5
Bipolar-byte <Dimanza33@yandex.ru>
2024-08-21 04:05:13 +0300

    Add files via upload

--- Leason2.py
+++ Leason2.py
@@ -1,5 +1,9 @@
-lesson = 'Python'
-The_number_of_completed_DZ = '12'
-Number_of_hours_spent = "1.5"
-Time_for_one_task = '0.125'
-print("Курс",lesson,'всего задач:',The_number_of_completed_DZ,'затрачено часов:',Number_of_hours_spent,'среднее время выполнения:',Time_for_one_task)
+lesson = 'Курс: '
+type_lesson = "Python"
+The_number_of_completed_DZ = 'Количество выполненных ДЗ:'
+Count_quest = 12
+Number_of_hours_spent = "Количество затраченных часов:"
+Time_count = 1.5
+Quest_time = "Время на одно задание: "
+Time_quest = Time_count / Count_quest
+print(lesson, type_lesson, The_number_of_completed_DZ, Count_quest, Number_of_hours_spent, Time_count, Time_quest)
